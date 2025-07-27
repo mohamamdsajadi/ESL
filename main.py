fcfc7e7134d74ee44b2a91a539039092f8d0ee80
mohammadsajadi <mohammadsajadi50@gmail.com>
2025-07-27 15:04:44 +0330

    change

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import ESL
 con = ESL.ESLconnection("127.0.0.1", "8021", "eba1395137fb49d1")
 
 if con.connected():
-    print("✅ Connected to FreeSWITCH ESL interface!")
-
-    con.events("plain", "ALL")
+    con.events("plain", "CHANNEL_BRIDGE")
 
     while True:
         e = con.recvEvent()
         if e:
-            print(f"\n📦 EVENT: {e.serialize()}")
-else:
-    print("❌ Failed to connect to FreeSWITCH.")
+            uuid = e.getHeader("Unique-ID")
+            user_id = e.getHeader("variable_bbb_user_id")
+            meeting_id = e.getHeader("variable_bbb_meeting_id")
+            print("UUID", uuid, "USER_ID: ", user_id, "MEETING_ID", meeting_id)
+
+            # Construct your audio stream target
+            ws_url = f"ws://46.245.79.23:9000/ws/audio?userId={user_id}&meetingId={meeting_id}"
+            fork_cmd = f"uuid_audio_fork {uuid} start {ws_url} mono 16000"
+
+            # Send the command to FreeSWITCH
+            con.api(fork_cmd)
+            print("forked")
