5454b84f8de019fea26aaf3449568605a3edc5f9
mohammadsajadi <mohammadsajadi50@gmail.com>
2025-12-21 18:43:33 +0330

    add a new file to test caption sending meeting_id is conference name

--- main.py
+++ main.py
@@ -32,28 +32,28 @@ if con.connected():
         if not user_id:
            continue
         user_name: str = e.getHeader("Caller-Caller-ID-Name").replace(user_id+"-bbbID-", "")
-        variable_conference_name = e.getHeader("Conference-Unique-ID")  # bbb variable conf name
+        conference_name = e.getHeader("Conference-Name")  # bbb variable conf name
         speak = e.getHeader("Speak")  # "true" / "false"
 
-        print("special log",variable_conference_name, user_name, user_id)
+        print("special log", conference_name, user_name, user_id)
 
         # Ensure required fields are present
-        if not uuid or not user_id or not variable_conference_name:
-            print("no uuid or user_id or variable_conference_name")
+        if not uuid or not user_id or not conference_name:
+            print("no uuid or user_id or conference_name")
             continue
 
         # ✅ User is unmuted — start audio fork
         if action == "unmute-member" and speak == "true" and uuid not in forked_uuids:
-            ws_url = f"ws://46.245.79.23:9000/ws/audio?user_id={user_id}&meeting_id={variable_conference_name}&user_name={user_name}"
+            ws_url = f"ws://46.245.79.23:9000/ws/audio?user_id={user_id}&conference_name={conference_name}&user_name={user_name}"
             fork_cmd = f"uuid_audio_fork {uuid} start {ws_url} mono 16000"
             res =  con.bgapi(fork_cmd)
             print(res.getBody())
             forked_uuids.add(uuid)
-            print(f"[Fork Started] {user_id=} {variable_conference_name=} {uuid=}")
+            print(f"[Fork Started] {user_id=} {conference_name=} {uuid=}")
 
         # 🔴 User is muted — stop audio fork
         elif action == "mute-member" and speak == "false" and uuid in forked_uuids:
             stop_cmd = f"uuid_audio_fork {uuid} stop"
             con.api(stop_cmd)
             forked_uuids.remove(uuid)
-            print(f"[Fork Stopped] {user_id=} {variable_conference_name=} {uuid=}")
+            print(f"[Fork Stopped] {user_id=} {conference_name=} {uuid=}")
